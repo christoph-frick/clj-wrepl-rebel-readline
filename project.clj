4988e872e83a56497aa4432f3ed2222761d6bec9
Christoph Frick <cf@ofnir.net>
2018-07-21 10:21:28 +0200

    Update rebel-readline to 0.1.4

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject net.ofnir/wrepl.rebel-readline "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.bhauman/rebel-readline "0.1.2"]])
+                 [com.bhauman/rebel-readline "0.1.4"]])
