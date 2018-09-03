6459f1071956d410e69cd16cbe2a57628124218a
Christoph Frick <cf@ofnir.net>
2018-09-03 18:50:35 +0200

    Add missing integrant dep

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject net.ofnir/wrepl.rebel-readline "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.bhauman/rebel-readline "0.1.4"]])
+                 [com.bhauman/rebel-readline "0.1.4"]
+                 [integrant "0.6.3"]])
