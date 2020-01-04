4b777785efb7cfe24b053d9d66b7a347defb64d0
Christoph Frick <cf@ofnir.net>
2020-01-04 13:59:40 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir/wrepl.rebel-readline "0.1.0"
+(defproject net.ofnir/wrepl.rebel-readline "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [integrant "0.7.0"]]
