ae5166c1c9527f0c11ad1c6e4f8fabe6e76109f6
Christoph Frick <cf@ofnir.net>
2020-02-11 12:32:09 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/wrepl.rebel-readline "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.bhauman/rebel-readline "0.1.4"]
-                 [integrant "0.7.0"]]
+                 [integrant "0.8.0"]]
   :deploy-repositories [["releases" :clojars]])
