afeece55d183c70d6a86ea55f05c9652994171e4
Christoph Frick <cf@ofnir.net>
2022-06-07 17:24:14 +0200

    Update to Clojure 1.11

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/wrepl.rebel-readline "0.1.2-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.bhauman/rebel-readline "0.1.4"]
                  [integrant "0.8.0"]]
   :deploy-repositories {"releases" {:url "https://clojars.org/repo/" :creds :gpg}})
